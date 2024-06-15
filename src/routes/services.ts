3807a60d860635d4fb9b5750b264a1a4d727906a
Manish Roy <manish.11612939@lpu.in>
2024-06-15 18:58:19 +0530

    added search route for guides

--- src/routes/services.ts
+++ src/routes/services.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import validator from '../middlewares/validator'
 import { getRecommendationsValidationRules } from '../schemas/services'
 import { ServicesController } from '../controllers'
+import { setUserPreferencesValidationRules } from '../schemas/user'
 
 const router = express.Router()
 
@@ -32,6 +33,62 @@ const router = express.Router()
 router.get(
   '/recommendations',
   getRecommendationsValidationRules,
+  validator,
+  ServicesController.getRecommendations,
+)
+
+/**
+ * @swagger
+ * /services/recommendations/guides:
+ *   post:
+ *     summary: Get recommendations for users based on input parameteres.
+ *     description: Note - These input parameters will have higher priority than the stored preferences of the user.
+ *     tags:
+ *       - Services
+ *     security:
+ *       - JWTAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               countryCode:
+ *                 type: string
+ *                 description: must be 'ISO 3166-1 alpha-3' code
+ *               regionIds:
+ *                 type: array
+ *                 items:
+ *                   type: integer
+ *                 example: [1, 2, 3]
+ *               levelIds:
+ *                 type: array
+ *                 items:
+ *                   type: integer
+ *                 example: [1]
+ *               courseIds:
+ *                 type: array
+ *                 items:
+ *                   type: integer
+ *                 example: [2]
+ *               universityIds:
+ *                 type: array
+ *                 items:
+ *                   type: integer
+ *                 example: []
+ *               isActive:
+ *                 type: boolean
+ *                 description: Optional - true by default
+ *             required:
+ *               - countryCode
+ *     responses:
+ *       200:
+ *         $ref: '#/components/responses/SuccessResponse'
+ */
+router.post(
+  '/recommendations/guides',
+  setUserPreferencesValidationRules,
   validator,
   ServicesController.getRecommendations,
 )
