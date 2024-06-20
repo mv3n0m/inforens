f980a849041e9e96b043879ec3bfd425caeb492f
Manish Roy <manish.11612939@lpu.in>
2024-06-21 02:13:28 +0530

    allowed all cors

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,9 @@ import { swaggerSpec } from './swagger'
 const app = express()
 
 app.use(helmet())
-app.use(express.json())
+app.use(express.json({ limit: '5mb' }))
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(morgan('combined'))
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
